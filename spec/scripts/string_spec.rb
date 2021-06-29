5e12e3cf1f5e7d5dff99f7dbccd9a345365a0429
Jelani Woods <jelani@firstdraft.com>
2021-06-29 16:11:42 -0500

    Update tests

--- spec/scripts/string_spec.rb
+++ spec/scripts/string_spec.rb
@@ -8,31 +8,42 @@ describe "string_multiplication.rb" do
           "Expected 'string_multiplication.rb' to NOT print the String literal 'HoHoHo', but did."
       end
     end
-    expect { require_relative '../../string_multiplication' }.to output(/HoHoHo/).to_stdout
+    output = with_captured_stdout { require_relative('../../string_multiplication')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(/HoHoHo/)).to be(true), "Expected output to be 'HoHoHo', but was #{output}"
   end
 end
 
 describe "string_strip.rb" do
   it "should output 'remove outside spaces'", points: 1 do
-    expect { require_relative '../../string_strip' }.to output(/remove the outside spaces/).to_stdout
+
+    output = with_captured_stdout { require_relative('../../string_strip')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(/remove the outside spaces/)).to be(true), "Expected output to be 'remove the outside spaces', but was #{output}"
   end
 end
 
 describe "string_case.rb" do
   it "should output 'HELLO friends AnD FaMiLy'", points: 1 do
-    expect { require_relative '../../string_case' }.to output(/HELLO friends AnD FaMiLy/).to_stdout
+    output = with_captured_stdout { require_relative('../../string_case')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(/HELLO friends AnD FaMiLy/)).to be(true), "Expected output to be 'HELLO friends AnD FaMiLy', but was #{output}."
   end
 end
 
 describe "string_gsub.rb" do
   it "should output 'put spaces in between these words'", points: 1 do
-    expect { require_relative '../../string_gsub' }.to output(/put spaces in between these words/).to_stdout
+    output = with_captured_stdout { require_relative('../../string_gsub')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(/put spaces in between these words/)).to be(true), "Expected output to be 'put spaces in between these words', but was #{output}."
   end
 end
 
 describe "string_chomp.rb" do
   it "should remove ending '$'", points: 1 do
-    expect { require_relative '../../string_chomp' }.to output(/Hello!/).to_stdout
+    output = with_captured_stdout { require_relative('../../string_chomp')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(/Hello!/)).to be(true), "Expected output to be 'Hello!', but was #{output}."
   end
 end
 
@@ -40,6 +51,17 @@ describe "string_gets.rb" do
   it "should output 'Hello, name!'" , points: 1 do
     allow_any_instance_of(Object).to receive(:gets).and_return("jelani\n")
 
-    expect { require_relative '../../string_gets' }.to output(/Hello, Jelani!/).to_stdout
+    output = with_captured_stdout { require_relative('../../string_gets')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(/Hello, Jelani!/)).to be(true), "Expected output to be 'Hello, Jelani!', but was #{output}."
   end
+end
+
+def with_captured_stdout
+  original_stdout = $stdout  # capture previous value of $stdout
+  $stdout = StringIO.new     # assign a string buffer to $stdout
+  yield                      # perform the body of the user code
+  $stdout.string             # return the contents of the string buffer
+ensure
+  $stdout = original_stdout  # restore $stdout to its previous value
 end
